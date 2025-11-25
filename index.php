86f6a481c707e6eb720ea84546c42d362441f2fd
GueldenBayar <g.bayar@hotmail.de>
2025-11-25 12:00:08 +0100

    new navbar in index and new style.css file

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'db.php';
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Pawplanner - Home</title>
+</head>
+<body>
+<h2>Pawplanner - Match your furry friends!</h2>
+
+<ul>
+    <li><a href="">Home</a></li>
+    <li><a href="">Furry Match</a></li>
+    <li><a href="">Furr Friendly Map</a></li>
+    <li><a href="">Profile</a></li>
+</ul>
+</body>
+</html>
